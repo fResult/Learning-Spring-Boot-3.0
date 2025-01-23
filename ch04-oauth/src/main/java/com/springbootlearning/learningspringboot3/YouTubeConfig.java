ef76b9f99c415120ac7589e1486d69eb816b6584
fResult <styxmaz@gmail.com>
2025-01-24 06:45:04 +0700

    Create `webClient` in `YoutubeConfig` class.

--- ch04-oauth/src/main/java/com/springbootlearning/learningspringboot3/YouTubeConfig.java
+++ ch04-oauth/src/main/java/com/springbootlearning/learningspringboot3/YouTubeConfig.java
@@ -1,4 +1,25 @@
 package com.springbootlearning.learningspringboot3;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
 public class YouTubeConfig {
+  static final String YOUTUBE_V3_API = "https://www.googleapis.com/youtube/v3";
+
+  @Bean
+  public WebClient webClient(OAuth2AuthorizedClientManager oAuth2AuthorizedClientManager) {
+    final var oAuth2 =
+        new ServletOAuth2AuthorizedClientExchangeFilterFunction(oAuth2AuthorizedClientManager);
+
+    oAuth2.setDefaultClientRegistrationId("google");
+
+    return WebClient.builder() //
+        .baseUrl(YOUTUBE_V3_API)
+        .apply(oAuth2.oauth2Configuration())
+        .build();
+  }
 }
