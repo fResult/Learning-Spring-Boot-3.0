55f91eaacc8a75b2a04081c3045d12b865e0b6d3
fResult <styxmaz@gmail.com>
2025-01-24 06:25:42 +0700

    Create `oAuth2AuthorizedClientManager` bean.

--- ch04-oauth/src/main/java/com/springbootlearning/learningspringboot3/SecurityConfig.java
+++ ch04-oauth/src/main/java/com/springbootlearning/learningspringboot3/SecurityConfig.java
@@ -1,3 +1,32 @@
 package com.springbootlearning.learningspringboot3;
 
-public class SecurityConfig {}
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+
+@Configuration
+public class SecurityConfig {
+  @Bean
+  public OAuth2AuthorizedClientManager oAuth2AuthorizedClientManager(
+      ClientRegistrationRepository clientRegRepository,
+      OAuth2AuthorizedClientRepository authClientRepository) {
+
+    final var clientManager =
+        new DefaultOAuth2AuthorizedClientManager(clientRegRepository, authClientRepository);
+    final var clientProvider =
+        OAuth2AuthorizedClientProviderBuilder.builder()
+            .authorizationCode()
+            .refreshToken()
+            .clientCredentials()
+            .password()
+            .build();
+
+    clientManager.setAuthorizedClientProvider(clientProvider);
+
+    return clientManager;
+  }
+}
