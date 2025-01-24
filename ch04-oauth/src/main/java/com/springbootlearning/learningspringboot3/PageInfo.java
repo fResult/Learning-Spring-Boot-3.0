29fcd4c28195d25615a4ed1bf6cca25f5fce4cf9
fResult <styxmaz@gmail.com>
2025-01-25 00:21:49 +0700

    Update java doc in `PageInfo` record class.

--- ch04-oauth/src/main/java/com/springbootlearning/learningspringboot3/PageInfo.java
+++ ch04-oauth/src/main/java/com/springbootlearning/learningspringboot3/PageInfo.java
@@ -1,6 +1,6 @@
 package com.springbootlearning.learningspringboot3;
 
 /**
- * @see https://developers.google.com/youtube/v3/docs/videos/list
+ * @see <a href="https://developers.google.com/youtube/v3/docs/videos/list">YouTube Data API - Videos: list</a>
  */
 record PageInfo(Integer totalResults, Integer resultsPerPage) {}
