04fafdd22c0bc828299954ded22764c2047fdc6c
fResult <styxmaz@gmail.com>
2025-01-25 19:57:04 +0700

    Create `proxyFactory` and `youTubeClient` beans.

--- ch04-oauth/src/main/java/com/springbootlearning/learningspringboot3/YouTubeConfig.java
+++ ch04-oauth/src/main/java/com/springbootlearning/learningspringboot3/YouTubeConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @Configuration
 public class YouTubeConfig {
@@ -21,5 +23,15 @@ public class YouTubeConfig {
         .baseUrl(YOUTUBE_V3_API)
         .apply(oAuth2.oauth2Configuration())
         .build();
+  }
+
+  @Bean
+  HttpServiceProxyFactory proxyFactory(WebClient webClient) {
+    return HttpServiceProxyFactory.builder().clientAdapter(WebClientAdapter.forClient(webClient)).build();
+  }
+
+  @Bean
+  YouTube youTubeClient(HttpServiceProxyFactory proxyFactory) {
+    return proxyFactory.createClient(YouTube.class);
   }
 }
