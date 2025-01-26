cab824b73267cf2596cb62960efc46763bdb00ab
fResult <styxmaz@gmail.com>
2025-01-26 23:58:28 +0700

    Create `getVideosShouldReturnAll` test case.

--- ch05/src/test/java/com/springbootlearning/learningspringboot3/VideoServiceTest.java
+++ ch05/src/test/java/com/springbootlearning/learningspringboot3/VideoServiceTest.java
@@ -1,7 +1,11 @@
 package com.springbootlearning.learningspringboot3;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -15,5 +19,19 @@ class VideoServiceTest {
   @BeforeEach
   void setUp() {
     this.videoService = new VideoService(videoRepository);
+  }
+
+  @Test
+  void getVideosShouldReturnAll() {
+    // Given
+    final var video1 = new VideoEntity("alice", "video 1", "description");
+    final var video2 = new VideoEntity("alice", "video 2", "descriptionnnn");
+    when(videoRepository.findAll()).thenReturn(List.of(video1, video2));
+
+    // When
+    final var videos = videoService.getVideos();
+
+    // Then
+    assertThat(videos).containsExactly(video1, video2);
   }
 }
