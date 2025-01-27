2917f8c8e61a9434284174f05b96b76231d4c223
fResult <styxmaz@gmail.com>
2025-01-27 12:22:45 +0700

    Create `deleteVideoShouldWork` test case.

--- ch05/src/test/java/com/springbootlearning/learningspringboot3/VideoServiceTest.java
+++ ch05/src/test/java/com/springbootlearning/learningspringboot3/VideoServiceTest.java
@@ -3,9 +3,11 @@ package com.springbootlearning.learningspringboot3;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -52,5 +54,21 @@ class VideoServiceTest {
     assertThat(actualCreatedVideo.getName()).isEqualTo("video 1");
     assertThat(actualCreatedVideo.getUsername()).isEqualTo("alice");
     assertThat(actualCreatedVideo.getDescription()).isEqualTo("description");
+  }
+
+  @Test
+  void deleteVideoShouldWork() {
+    // Given
+    final var videoId = 1L;
+    final var videoToDelete = new VideoEntity("alice", "video 1", "description");
+    videoToDelete.setId(videoId);
+    when(videoRepository.findById(1L)).thenReturn(Optional.of(videoToDelete));
+
+    // When
+    videoService.delete(videoId);
+
+    // Then
+    verify(videoRepository).findById(videoId);
+    verify(videoRepository).delete(videoToDelete);
   }
 }
