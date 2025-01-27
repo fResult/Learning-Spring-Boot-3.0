8cd5e9bcea03008f959ff0a05d7e51a1a81b2cc4
fResult <styxmaz@gmail.com>
2025-01-27 14:46:27 +0700

    Update data for the `VideoRepositoryTest.setUp()`.

--- ch05/src/test/java/com/springbootlearning/learningspringboot3/VideoRepositoryTest.java
+++ ch05/src/test/java/com/springbootlearning/learningspringboot3/VideoRepositoryTest.java
@@ -14,15 +14,28 @@ class VideoRepositoryTest {
 
   @BeforeEach
   void setUp() {
-    final var video1 = new VideoEntity("user", "video1", "video1");
-    final var video2 = new VideoEntity("user", "video2", "video2");
-    final var video3 = new VideoEntity("user", "video3", "video3");
+    final var video1 =
+        new VideoEntity(
+            "user",
+            "Need HELP with your SPRING BOOT 3 App?",
+            "SPRING BOOT 3 will only speed things up.");
+    final var video2 =
+        new VideoEntity(
+            "user",
+            "Don't do THIS to your own CODE!",
+            "As a pro developer, never ever EVER do this to your code.");
+    final var video3 =
+        new VideoEntity(
+            "user", "SECRETS to fix BROKEN CODE!", "Discover ways to not only debug your code");
     videoRepository.saveAll(List.of(video1, video2, video3));
   }
 
   @Test
   void findAllByUsernameShouldReturnAllVideosForUser() {
+    // When
     final var videos = videoRepository.findAll();
+
+    // Then
     assertThat(videos).hasSize(3);
   }
 }
