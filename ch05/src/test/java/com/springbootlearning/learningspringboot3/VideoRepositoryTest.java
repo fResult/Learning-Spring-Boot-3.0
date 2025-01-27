f93b13dc616f977cb7089679c5561b9000c2cb91
fResult <styxmaz@gmail.com>
2025-01-27 13:53:58 +0700

    Create `findAllByUsernameShouldReturnAllVideosForUser` test case.

--- ch05/src/test/java/com/springbootlearning/learningspringboot3/VideoRepositoryTest.java
+++ ch05/src/test/java/com/springbootlearning/learningspringboot3/VideoRepositoryTest.java
@@ -1,7 +1,10 @@
 package com.springbootlearning.learningspringboot3;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
@@ -15,5 +18,11 @@ class VideoRepositoryTest {
     final var video2 = new VideoEntity("user", "video2", "video2");
     final var video3 = new VideoEntity("user", "video3", "video3");
     videoRepository.saveAll(List.of(video1, video2, video3));
+  }
+
+  @Test
+  void findAllByUsernameShouldReturnAllVideosForUser() {
+    final var videos = videoRepository.findAll();
+    assertThat(videos).hasSize(3);
   }
 }
