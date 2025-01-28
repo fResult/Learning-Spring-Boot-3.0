010e6dc49b08817ab7f7d63964d36a14a876dd02
fResult <styxmaz@gmail.com>
2025-01-29 01:52:53 +0700

    Create `unauthenticatedUserShouldNotCreateNewVideo` test case.

--- ch05/src/test/java/com/springbootlearning/learningspringboot3/SecurityBasedTest.java
+++ ch05/src/test/java/com/springbootlearning/learningspringboot3/SecurityBasedTest.java
@@ -1,6 +1,8 @@
 package com.springbootlearning.learningspringboot3;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
@@ -31,5 +33,16 @@ class SecurityBasedTest {
   @WithMockUser(username = "admin", roles = "ADMIN")
   void authAdminShouldAccessHomePage() throws Exception {
     mockMvc.perform(get("/")).andExpect(status().isOk());
+  }
+
+  @Test
+  void unauthenticatedUserShouldNotCreateNewVideo() throws Exception {
+    mockMvc
+        .perform(
+            post("/new-video")
+                .param("name", "New Video")
+                .param("description", "new desc")
+                .with(csrf()))
+        .andExpect(status().isUnauthorized());
   }
 }
