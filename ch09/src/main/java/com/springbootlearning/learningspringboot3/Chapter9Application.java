ffdab0030b6df67eabea62506e25767238b556e8
fResult <styxmaz@gmail.com>
2025-02-13 03:59:24 +0700

    Re-format code.

--- ch09/src/main/java/com/springbootlearning/learningspringboot3/Chapter9Application.java
+++ ch09/src/main/java/com/springbootlearning/learningspringboot3/Chapter9Application.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Chapter9Application {
-
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     SpringApplication.run(Chapter9Application.class, args);
   }
 }
--- ch09/src/main/java/com/springbootlearning/learningspringboot3/entities/Employee.java
+++ ch09/src/main/java/com/springbootlearning/learningspringboot3/entities/Employee.java
@@ -1,7 +1,5 @@
 package com.springbootlearning.learningspringboot3.entities;
 
-import java.util.function.Function;
-
 public record Employee(Long id, String name, String role) {
   public Employee(String name, String role) {
     this(null, name, role);
