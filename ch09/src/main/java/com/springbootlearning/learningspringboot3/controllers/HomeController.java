64b9bd21e4796a15ca0270f19090e466386020e0
fResult <styxmaz@gmail.com>
2025-02-15 14:36:18 +0700

    Create `Add New Employee` form in Home page.

--- ch09/src/main/java/com/springbootlearning/learningspringboot3/controllers/HomeController.java
+++ ch09/src/main/java/com/springbootlearning/learningspringboot3/controllers/HomeController.java
@@ -1,18 +1,20 @@
 package com.springbootlearning.learningspringboot3.controllers;
 
 import com.springbootlearning.learningspringboot3.entities.Employee;
-
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.reactive.result.view.Rendering;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Controller
 public class HomeController {
+  private AtomicLong idGenerator = new AtomicLong(DATABASE.size());
   private static final Map<String, Employee> DATABASE =
       new HashMap<>() {
         {
@@ -25,6 +27,22 @@ public class HomeController {
   public Mono<Rendering> index() {
     return Flux.fromIterable(DATABASE.values())
         .collectList()
-        .map(employees -> Rendering.view("index").modelAttribute("employees", employees).build());
+        .map(
+            employees ->
+                Rendering.view("index")
+                    .modelAttribute("employees", employees)
+                    .modelAttribute("newEmployee", new Employee(null, "", ""))
+                    .build());
+  }
+
+  @PostMapping("/new-employee")
+  Mono<String> newEmployee(@ModelAttribute Mono<Employee> newEmployee) {
+    return newEmployee.doOnNext(this::saveEmployee).map(employee -> "redirect:/");
+  }
+
+  private void saveEmployee(Employee employee) {
+    DATABASE.put(
+        employee.name(),
+        new Employee(idGenerator.incrementAndGet(), employee.name(), employee.role()));
   }
 }
