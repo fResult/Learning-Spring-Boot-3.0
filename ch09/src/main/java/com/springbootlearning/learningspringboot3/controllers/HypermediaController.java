7f108071e7d3696d734e3e5bf02dc607121373e0
fResult <styxmaz@gmail.com>
2025-02-16 20:18:06 +0700

    Create GET `/hypermedia/employees/:id` API endpoint.

---
+++ ch09/src/main/java/com/springbootlearning/learningspringboot3/controllers/HypermediaController.java
@@ -0,0 +1,46 @@
+package com.springbootlearning.learningspringboot3.controllers;
+
+import static org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.methodOn;
+
+import com.springbootlearning.learningspringboot3.entities.Employee;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/hypermedia/employees")
+public class HypermediaController {
+  private final Map<Long, Employee> DATABASE =
+      new HashMap<>() {
+        {
+          put(1L, new Employee(1L, "Frodo Baggins", "Ring-bearer"));
+          put(2L, new Employee(2L, "Samwise Gamgee", "Gardener"));
+          put(3L, new Employee(3L, "Bilbo Baggins", "Burglar"));
+        }
+      };
+  private AtomicLong idGenerator = new AtomicLong(DATABASE.size());
+
+  @GetMapping("/{id}")
+  public Mono<EntityModel<Employee>> employeeById(@PathVariable Long id) {
+    final var selfLinkMono =
+        linkTo(methodOn(HypermediaController.class).employeeById(id)).withSelfRel().toMono();
+
+    return selfLinkMono.map(
+        selfLink ->
+            EntityModel.of(
+                Optional.of(DATABASE.get(id))
+                    .orElseThrow(
+                        () ->
+                            new ResponseStatusException(
+                                HttpStatus.NOT_FOUND,
+                                String.format("Employee with ID %d not found", id))),
+                selfLink));
+  }
+}
