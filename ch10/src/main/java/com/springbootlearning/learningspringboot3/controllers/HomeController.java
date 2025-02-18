ddb350f3483b355f0224b3767f5dcc43833afa13
fResult <styxmaz@gmail.com>
2025-02-19 01:41:05 +0700

    Refactor by extracting method to `toHomeRendering`.

--- ch10/src/main/java/com/springbootlearning/learningspringboot3/controllers/HomeController.java
+++ ch10/src/main/java/com/springbootlearning/learningspringboot3/controllers/HomeController.java
@@ -2,6 +2,7 @@ package com.springbootlearning.learningspringboot3.controllers;
 
 import com.springbootlearning.learningspringboot3.entities.Employee;
 import com.springbootlearning.learningspringboot3.repositories.EmployeeRepository;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -19,19 +20,18 @@ public class HomeController {
 
   @GetMapping("/")
   public Mono<Rendering> home() {
-    return employeeRepository
-        .findAll()
-        .collectList()
-        .map(
-            employees ->
-                Rendering.view("index")
-                    .modelAttribute("employees", employees)
-                    .modelAttribute("newEmployee", Employee.withoutId("", ""))
-                    .build());
+    return employeeRepository.findAll().collectList().map(this::toHomeRendering);
   }
 
   @PostMapping("/new-employee")
   Mono<String> createEmployee(@ModelAttribute("newEmployee") Mono<Employee> bodyMono) {
     return bodyMono.flatMap(employeeRepository::save).thenReturn("redirect:/");
+  }
+
+  private Rendering toHomeRendering(List<Employee> employees) {
+    return Rendering.view("index")
+        .modelAttribute("employees", employees)
+        .modelAttribute("newEmployee", Employee.withoutId("", ""))
+        .build();
   }
 }
