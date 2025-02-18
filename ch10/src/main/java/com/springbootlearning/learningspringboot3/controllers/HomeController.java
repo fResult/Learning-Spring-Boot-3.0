863762c09eafbf9d8a73c259157ba268d54244a2
fResult <styxmaz@gmail.com>
2025-02-19 01:35:56 +0700

    Create Home page with employees rendering.

--- ch10/src/main/java/com/springbootlearning/learningspringboot3/controllers/HomeController.java
+++ ch10/src/main/java/com/springbootlearning/learningspringboot3/controllers/HomeController.java
@@ -1,7 +1,11 @@
 package com.springbootlearning.learningspringboot3.controllers;
 
+import com.springbootlearning.learningspringboot3.entities.Employee;
 import com.springbootlearning.learningspringboot3.repositories.EmployeeRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.reactive.result.view.Rendering;
+import reactor.core.publisher.Mono;
 
 @Controller
 public class HomeController {
@@ -9,5 +13,18 @@ public class HomeController {
 
   public HomeController(EmployeeRepository employeeRepository) {
     this.employeeRepository = employeeRepository;
+  }
+
+  @GetMapping("/")
+  public Mono<Rendering> home() {
+    return employeeRepository
+        .findAll()
+        .collectList()
+        .map(
+            employees ->
+                Rendering.view("index")
+                    .modelAttribute("employees", employees)
+                    .modelAttribute("newEmployee", Employee.withoutId("", ""))
+                    .build());
   }
 }
