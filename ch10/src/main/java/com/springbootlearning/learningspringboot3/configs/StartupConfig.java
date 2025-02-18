137fbf200421c2e35a416ecbb4d38b0822f37a30
fResult <styxmaz@gmail.com>
2025-02-18 16:35:22 +0700

    Init `employees` table using `StartupConfig` class.

---
+++ ch10/src/main/java/com/springbootlearning/learningspringboot3/configs/StartupConfig.java
@@ -0,0 +1,26 @@
+package com.springbootlearning.learningspringboot3.configs;
+
+import com.springbootlearning.learningspringboot3.entities.Employee;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
+import reactor.test.StepVerifier;
+
+@Configuration
+public class StartupConfig {
+  @Bean
+  CommandLineRunner initEmployees(R2dbcEntityTemplate template) {
+    return args -> {
+      template
+          .getDatabaseClient()
+          .sql(
+              "CREATE TABLE IF NOT EXISTS employees (id IDENTITY NOT NULL PRIMARY KEY, name VARCHAR(255), role VARCHAR(255))")
+          .fetch()
+          .rowsUpdated()
+          .as(StepVerifier::create)
+          .expectNextCount(1)
+          .verifyComplete();
+    };
+  }
+}
