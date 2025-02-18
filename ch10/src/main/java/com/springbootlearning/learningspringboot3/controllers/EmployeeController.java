1d40718acf2647bf6b74699cfa7d3ecc8d571973
fResult <styxmaz@gmail.com>
2025-02-18 18:16:03 +0700

    Create PATCH `/api/employees/:id` API endpoint.

--- ch10/src/main/java/com/springbootlearning/learningspringboot3/controllers/EmployeeController.java
+++ ch10/src/main/java/com/springbootlearning/learningspringboot3/controllers/EmployeeController.java
@@ -1,9 +1,11 @@
 package com.springbootlearning.learningspringboot3.controllers;
 
+import com.springbootlearning.learningspringboot3.dtos.EmployeeUpdateRequest;
 import com.springbootlearning.learningspringboot3.entities.Employee;
 import com.springbootlearning.learningspringboot3.repositories.EmployeeRepository;
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
@@ -35,8 +37,31 @@ public class EmployeeController {
     return body.flatMap(this.employeeRepository::save).map(this::toCreatedResponseEntity);
   }
 
+  @PatchMapping("/{id}")
+  public Mono<ResponseEntity<Employee>> update(
+      @PathVariable Long id, @RequestBody Mono<EmployeeUpdateRequest> bodyMono) {
+
+    return employeeRepository
+        .findById(id)
+        .flatMap(
+            existingEmployee ->
+                bodyMono.map(
+                    body ->
+                        new Employee(
+                            id,
+                            Optional.of(body.name()).orElse(existingEmployee.name()),
+                            Optional.of(body.role()).orElse(existingEmployee.role()))))
+        .flatMap(employeeRepository::save)
+        .map(ResponseEntity::ok)
+        .switchIfEmpty(respondNotFoundMono());
+  }
+
   private ResponseEntity<Employee> toCreatedResponseEntity(Employee createdResource) {
     return ResponseEntity.created(URI.create("/api/employees/" + createdResource.id()))
         .body(createdResource);
+  }
+
+  private Mono<ResponseEntity<Employee>> respondNotFoundMono() {
+    return Mono.just(ResponseEntity.notFound().build());
   }
 }
