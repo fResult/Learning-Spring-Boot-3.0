292c714899f043a70cde3d462b201bd4c6707aad
fResult <styxmaz@gmail.com>
2025-02-19 01:37:32 +0700

    Create `/new-employee` working functionality with newEmployee form.

--- ch10/src/main/java/com/springbootlearning/learningspringboot3/controllers/HomeController.java
+++ ch10/src/main/java/com/springbootlearning/learningspringboot3/controllers/HomeController.java
@@ -4,6 +4,8 @@ import com.springbootlearning.learningspringboot3.entities.Employee;
 import com.springbootlearning.learningspringboot3.repositories.EmployeeRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.reactive.result.view.Rendering;
 import reactor.core.publisher.Mono;
 
@@ -26,5 +28,10 @@ public class HomeController {
                     .modelAttribute("employees", employees)
                     .modelAttribute("newEmployee", Employee.withoutId("", ""))
                     .build());
+  }
+
+  @PostMapping("/new-employee")
+  Mono<String> createEmployee(@ModelAttribute("newEmployee") Mono<Employee> bodyMono) {
+    return bodyMono.flatMap(employeeRepository::save).thenReturn("redirect:/");
   }
 }
