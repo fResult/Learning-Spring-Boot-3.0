35de2356b57412e63529066b18b401f2b478fdd5
fResult <styxmaz@gmail.com>
2025-02-18 11:58:15 +0700

    Prepare module 10 to be empty.

--- ch10/src/main/java/com/springbootlearning/learningspringboot3/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springbootlearning.learningspringboot3;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface EmployeeRepository
-    extends //
-    ReactiveCrudRepository<Employee, Long> {}
