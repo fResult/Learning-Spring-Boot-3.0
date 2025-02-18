5b6443005d7f862206efa0703ad17edba46af4fa
fResult <styxmaz@gmail.com>
2025-02-19 01:19:43 +0700

    Create HomeController.

--- ch10/src/main/java/com/springbootlearning/learningspringboot3/controllers/HomeController.java
+++ ch10/src/main/java/com/springbootlearning/learningspringboot3/controllers/HomeController.java
@@ -1,6 +1,13 @@
 package com.springbootlearning.learningspringboot3.controllers;
 
+import com.springbootlearning.learningspringboot3.repositories.EmployeeRepository;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class HomeController {}
+public class HomeController {
+  private final EmployeeRepository employeeRepository;
+
+  public HomeController(EmployeeRepository employeeRepository) {
+    this.employeeRepository = employeeRepository;
+  }
+}
