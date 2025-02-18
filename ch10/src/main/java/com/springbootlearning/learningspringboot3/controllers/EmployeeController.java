05c8874c0f1e2fa4df30c79614f811f1e5c81c84
fResult <styxmaz@gmail.com>
2025-02-18 17:10:45 +0700

    Create GET `/api/employees` API endpoint.

---
+++ ch10/src/main/java/com/springbootlearning/learningspringboot3/controllers/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.springbootlearning.learningspringboot3.controllers;
+
+import com.springbootlearning.learningspringboot3.entities.Employee;
+import com.springbootlearning.learningspringboot3.repositories.EmployeeRepository;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+  private final EmployeeRepository employeeRepository;
+
+  EmployeeController(EmployeeRepository employeeRepository) {
+    this.employeeRepository = employeeRepository;
+  }
+
+  @GetMapping
+  public Mono<ResponseEntity<List<Employee>>> all() {
+    return this.employeeRepository.findAll().collectList().map(ResponseEntity::ok);
+  }
+}
